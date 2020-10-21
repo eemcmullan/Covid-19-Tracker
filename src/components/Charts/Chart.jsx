c8d8a2717486d086199bf269d464a47c427d6c6c
Emily McMullan <eemcmullan92@gmail.com>
2020-10-21 10:23:08 -0400

    new files

---
+++ src/components/Charts/Chart.jsx
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+import {fetchDailyData} from '../../api';
+import {Line, Bar} from 'react-chartjs-2';
+import styles from './Chart.modules.css';
+
+const Chart = () => {
+	return (
+	<h1> Chart </h1>
+
+	)
+}
+
+export default Chart;
+
+
+
+
+
